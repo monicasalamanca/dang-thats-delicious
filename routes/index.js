22210ab8ed1fa0d86be04089895a0a138d952c1d
Monica Salamanca <moni.salamanca@gmail.com>
2020-01-15 19:08:16 -0500

    added new pug file to play with the layout and templating

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  //res.send('Hey! It works!');
+  // const test = {name:'monica',lastname:'salamanca',age:39};
+  // res.json(test);
+  //res.send(req.query.name);
+  // res.json(req.query);
+  res.render('hello');
 });
 
 module.exports = router;
