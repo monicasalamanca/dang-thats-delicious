71e089f11c7ed64949f78b20ddc3dfbe9adecd22
Monica Salamanca <moni.salamanca@gmail.com>
2020-01-19 21:03:41 -0500

    Query the db for store and displayed them on the home page

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-  console.log(req.name);
   res.render('index');
 }
 
@@ -14,4 +13,10 @@ exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save();
   req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
+}
+
+exports.getStores = async (req, res) => {
+  // 1. Query the db for a list of all stores
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores })
 }
