a507e3784b70fced2a5a43a2a0e93f3e846c8b3f
Monica Salamanca <moni.salamanca@gmail.com>
2020-01-25 18:53:04 -0500

    MongoDB aggregations

--- models/Store.js
+++ models/Store.js
@@ -49,10 +49,18 @@ storeSchema.pre('save', async function(next) {
   if(storesWithSlug.length) {
     this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
   }
-  
+
   next();
 
   // TODO make more resilient so slugs are more unique
 })
+
+storeSchema.statics.getTagsList = function() {
+  return this.aggregate([
+    { $unwind: '$tags' },
+    { $group: { _id: '$tags', count: { $sum: 1 } } },
+    { $sort: { count: -1 } }
+  ])
+}
 
 module.exports = mongoose.model('Store', storeSchema);
